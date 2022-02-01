7c53d8ed971a3519b333394f1082ff1da8018ff6
Anna <anlumithe@googlemail.com>
2022-02-01 14:22:44 +0100

    Transformed script into class definition

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # notionDBtoPandasDF
 
-This script converts a Notion database into a dataframe and saves it as Feather file. 
-All you have to do is run it and enter your integration token and your database id.
+With this code you can transform a Notion database into a Pandas dataframe.
+To create a new instance of the NotionDatabase class, you have to provide two parameters:
+1. The internal integration token: A tutorial on how an integration is created and how your database is shared with your integration can be found [here](https://developers.notion.com/docs/getting-started)(Step 1+2).
+2. The database ID: A detailed description on where to find the ID can also be found [here](https://developers.notion.com/docs/getting-started)(Step 2).
--- readDB.py
+++ readDB.py
@@ -1,40 +1,26 @@
 import requests, json
 import pandas as pd
 
-#first the user has to enter the integration token
-token = str(input('Enter secret token: '))
 
-#then the user has to enter the database id
-databaseId = str(input('Enter database id: '))
+class NotionDatabase(): 
 
+    def __init__(self, token, database_id) -> None:
+        self.token = token
+        self.database_id = database_id
+        self.headers = {
+        "Authorization": "Bearer " + token,
+        "Notion-Version": "2021-05-13"
+        }
 
-headers = {
-    "Authorization": "Bearer " + token,
-    "Notion-Version": "2021-05-13"
-}
+    def to_dataframe(self):
+        read_url = f"https://api.notion.com/v1/databases/{self.database_id}/query"
 
-#reads data and transformes it into an dataframe
-def readDatabase(databaseId, headers):
-    readUrl = f"https://api.notion.com/v1/databases/{databaseId}"
+        #gets data of database
+        res = requests.request("POST", read_url, headers=self.headers)
+        data = res.json()
+        print('Status code for data request: ', res.status_code) # if status code is 200 everything is great
 
-    #gets general info about the database --> like title etc.
-    db_info = requests.request("GET", readUrl, headers=headers)
-    info = db_info.json()
-    print('Status code for info request: ', db_info.status_code) # if status code is 200 everything is great
+        df = pd.json_normalize(data, record_path=['results']) #converts json data into pandas dataframe
 
-    title = info['title'][0]['plain_text'] # save title for filename later
+        return df
 
-
-    #gets data of database
-    res = requests.request("POST", readUrl+'/query', headers=headers)
-    data = res.json()
-    print('Status code for data request: ', res.status_code) # if status code is 200 everything is great
-
-    df = pd.json_normalize(data, record_path=['results']) #converts json data into pandas dataframe
-    #print(df)
-
-    file_name = title+'_'+databaseId+'.feather' # creates filename out of tilte and database id
-    df.to_feather(file_name) # saves file as .feather
-    
-
-readDatabase(databaseId, headers)
